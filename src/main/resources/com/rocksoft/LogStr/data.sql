16047d3a8a7b0d1212644830d131c8e7e4535551
khegor <egor1234@gmail.com>
2017-09-07 23:10:41 +0300

    add mySQL model and data

---
+++ src/main/resources/com/rocksoft/LogStr/data.sql
@@ -0,0 +1,19 @@
+INSERT INTO mydb.ADDRESSES (COUNTRY, CITY, STREET, HOME_NUMBER) VALUES ('BLR', 'Minsk', 'Leukova', '3'),
+('BLR', 'Minsk', 'Leukova', '31'),
+('BLR', 'Hrodno', 'Mira', '2'),
+('BLR', 'Mohileu', 'Spartiunaja', '31'),
+('BLR', 'Vitebsk', 'Severnaja', '9'),
+('BLR', 'Gomel', 'Lenina', '16'),
+('BLR', 'Brest', 'Lenina', '10'),
+('BLR', 'Minsk', 'Volodko', '22'),
+('BLR', 'Babrujsk', 'Mira', '15'),
+('BLR', 'Orsha', 'Zadneprouskaja', '7'),
+('BLR', 'Minsk', 'Mahileuskaja', '8'),
+('BLR', 'Minsk', 'Prititskogo', '38');
+INSERT INTO mydb.DIRECTOR (NAME, SURNAME, ESTABLISHED_POST, ADDRESSES_ID, DATE_OF_BIRTH) VALUES ('Ivan', 'Petrov', 'Director', '1', '2013-08-05 18:19:03');
+INSERT INTO mydb.DRIVER (NAME, SURNAME, ESTABLISHED_POST, ADDRESSES_ID, DATE_OF_BIRTH) VALUES ('Ivan', 'Ivanov', 'Director', '2', '2013-08-05 18:19:03'),
+('Syarhey', 'Prosto', 'Director', '3', '2013-08-05 18:19:03');
+
+
+
+
