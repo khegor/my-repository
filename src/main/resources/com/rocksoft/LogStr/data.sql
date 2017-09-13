29815a803f67261a32bb56cc8f40a70873d373b0
khegor <egor1234@gmail.com>
2017-09-13 14:27:07 +0300

    select using "FROM", "WHERE"

--- src/main/resources/com/rocksoft/LogStr/data.sql
+++ src/main/resources/com/rocksoft/LogStr/data.sql
@@ -1,5 +1,5 @@
 USE mydb;
-INSERT INTO MYDB.addresses (COUNTRY, CITY, STREET, HOME_NUMBER) VALUES ("BLR", "Minsk", "Leukova", 1),
+INSERT INTO addresses (COUNTRY, CITY, STREET, HOME_NUMBER) VALUES ("BLR", "Minsk", "Leukova", 1),
 ("BLR", "Minsk", "Leukova", 3),
 ("BLR", "Minsk", "Mohileuskaya", 1),
 ("BLR", "Minsk", "Mohileuskaya", 2),
@@ -16,27 +16,27 @@ INSERT INTO MYDB.addresses (COUNTRY, CITY, STREET, HOME_NUMBER) VALUES ("BLR", "
 ("BLR", "Minsk", "Dzerjinskogo", 126),
 ("BLR", "Minsk", "Dzerjinskogo", 21),
 ("BLR", "Minsk", "Asanalieva", 31);
-INSERT INTO MYDB.drivers_cars (CAR_MODEL, NUMBER) VALUES ("Mercedes", "0406-7"),
+INSERT INTO drivers_cars (CAR_MODEL, NUMBER) VALUES ("Mercedes", "0406-7"),
 ("Volvo", "0203-7"),
 ("Lada", "0304-7"),
 ("Peugeot", "0774-7"),
 ("Peugeot", "0474-7"),
 ("Volkswagen", "9988-7"),
 ("Renault", "2223-7");
-INSERT INTO MYDB.DRIVERS (NAME, SURNAME, ESTABLISHED_POST, DATE_OF_BIRTH, ADDRESSES_ID) VALUES ("Alex", "Ivanoff", "driver", "1992-02-07", 1),
+INSERT INTO DRIVERS (NAME, SURNAME, ESTABLISHED_POST, DATE_OF_BIRTH, ADDRESSES_ID) VALUES ("Alex", "Ivanoff", "driver", "1992-02-07", 1),
 ("Sima", "Endelson", "driver", "1969-01-01", 2),
 ("David", "Sarafyan", "driver", "1988-05-12", 3),
 ("Ivan", "Ivanoff", "driver", "1981-08-24", 4),
 ("Alex", "Smirnoff", "driver", "1980-05-30", 6),
 ("Zurab", "Martinaschvily", "driver", "1988-11-26", 5);
-INSERT INTO MYDB.DIRECTORS (NAME, SURNAME, ESTABLISHED_POST, DATE_OF_BIRTH, ADDRESSES_ID) VALUES ("Solomon", "Saroumjan", "director", "1990-09-17", 7);
-INSERT INTO MYDB.HEADS_OF_DEPARTMENTS (NAME, SURNAME, ESTABLISHED_POST, DATE_OF_BIRTH, ADDRESSES_ID) VALUES ("Mikhail", "Zadornov", "head_of_department", "1964-12-10", 8);
-INSERT INTO MYDB.LOGISTS (NAME, SURNAME, ESTABLISHED_POST, DATE_OF_BIRTH, ADDRESSES_ID) VALUES ("Mikhail", "Saakashvily", "logist", "1978-01-10", 11),
+INSERT INTO DIRECTORS (NAME, SURNAME, ESTABLISHED_POST, DATE_OF_BIRTH, ADDRESSES_ID) VALUES ("Solomon", "Saroumjan", "director", "1990-09-17", 7);
+INSERT INTO HEADS_OF_DEPARTMENTS (NAME, SURNAME, ESTABLISHED_POST, DATE_OF_BIRTH, ADDRESSES_ID) VALUES ("Mikhail", "Zadornov", "head_of_department", "1964-12-10", 8);
+INSERT INTO LOGISTS (NAME, SURNAME, ESTABLISHED_POST, DATE_OF_BIRTH, ADDRESSES_ID) VALUES ("Mikhail", "Saakashvily", "logist", "1978-01-10", 11),
 ("Dmitry", "Safonoff", "logist", "1992-04-10", 13);
-INSERT INTO MYDB.STOREKEEPERS (NAME, SURNAME, ESTABLISHED_POST, DATE_OF_BIRTH, ADDRESSES_ID) VALUES ("Rustam", "Ajbegulin", "storekeeper", "1993-04-01", 9),
+INSERT INTO STOREKEEPERS (NAME, SURNAME, ESTABLISHED_POST, DATE_OF_BIRTH, ADDRESSES_ID) VALUES ("Rustam", "Ajbegulin", "storekeeper", "1993-04-01", 9),
 ("Adolf", "Messer", "storekeeper", "1969-01-01", 10),
 ("Aram", "Safaryan", "storekeeper", "1988-05-12", 12);
-INSERT INTO MYDB.ORDERS (OWNER_ORGANISATION_NAME, OWNERSHIP_TYPE, OPERATION_TYPE, LOGISTS_ID) VALUES ("ASSTRA", "LTD", "TRANSPORTATION", 1);
+INSERT INTO ORDERS (OWNER_ORGANISATION_NAME, OWNERSHIP_TYPE, OPERATION_TYPE, LOGISTS_ID) VALUES ("ASSTRA", "LTD", "TRANSPORTATION", 1);
 ALTER TABLE drivers_cars COMMENT = 'alter table operations';
 ALTER TABLE drivers_cars ADD CAR_AGE INT NOT NULL;
 ALTER TABLE drivers_cars MODIFY CAR_AGE TINYINT NOT NULL DEFAULT '0';
@@ -48,7 +48,7 @@ INSERT INTO drivers_cars (CAR_MODEL, NUMBER, CAR_AGE) VALUES ("Alfa Romeo", "013
 ("Alfa Romeo", "9090-7", 4);
 UPDATE drivers_cars SET CAR_MODEL='BMW' WHERE ID=3;
 SELECT * FROM drivers_cars;
-
+SELECT ID, CAR_MODEL, CREATED_AT, CAR_AGE FROM drivers_cars WHERE (CAR_AGE < 1 && CAR_MODEL <> "BMW");
 
 
 
